6ef4c1194e36dda46e53616f5794047e36c028a3
ianba <ianbabington93@gmail.com>
2019-09-15 17:33:49 +1200

    changes for mobile support

--- src/components/backgroundCredit/backgroundCredit.js
+++ src/components/backgroundCredit/backgroundCredit.js
@@ -3,7 +3,7 @@ import './backgroundCredit.css';
 
 const backgroundCredit = () => {
     return (
-        <p className="backgroundCredit">Background by Sam Willis https://www.pexels.com/photo/photo-of-night-sky-1154610/</p>
+        <p className="backgroundCredit">Background by Sam Willis<br/>https://www.pexels.com/photo/photo-of-night-sky-1154610/</p>
     );
 }
 
