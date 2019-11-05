17df13588d4a90939a3d8759679cccd9e3058ab7
ianba <ianbabington93@gmail.com>
2019-11-06 00:11:20 +1300

    text change

--- src/components/pages/aboutMe/aboutMe.js
+++ src/components/pages/aboutMe/aboutMe.js
@@ -3,11 +3,13 @@ import './aboutMe.css'
 
 const headline = "About me"
 
-const intro = "In Agile teams at the Ministry of Social Development, I've built client facing websites, back end Client Management Systems and Deployment Pipelines in support of large Government projects."
+const intro = "As part of Agile teams at the Ministry of Social Development, I've built client facing websites, " 
+    + "backend Client Management Systems and Deployment Pipelines in support of large Government projects."
 
 const tech = "This was achieved using tech including Java, Javascipt and Python supported by both relational and NoSQL databases";
 
-const uni = "Studying Honours Computer Science and Information Systems at the University of Auckland, taught me technical and business skills required for modern software development. I also conducted original research into designing digital exercise games";
+const uni = "Studying Honours Computer Science and Information Systems at the University of Auckland, taught me the technical and business skills "
+    + "to succeed in modern software development. I also conducted original research into designing digital exercise games";
 
 const bio = () => { return(
     <div className="about">
